b195ee52c320915985387cc5b3ed6b44880adca1
willyrinaldi <73163225+willyrinaldi@users.noreply.github.com>
2023-09-21 09:55:30 +0700

    Create soal1.go

---
+++ soal1.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+    var n int
+    fmt.Print("Masukkan sebuah bilangan: ")
+    fmt.Scan(&n)
+
+    if isPrime(n) {
+        fmt.Printf("%d adalah bilangan prima\n", n)
+    } else {
+        fmt.Printf("%d bukan bilangan prima\n", n)
+    }
+}
+
+func isPrime(n int) bool {
+    if n <= 1 {
+        return false
+    }
+    if n <= 3 {
+        return true
+    }
+    if n%2 == 0 || n%3 == 0 {
+        return false
+    }
+    i := 5
+    for i*i <= n {
+        if n%i == 0 || n%(i+2) == 0 {
+            return false
+        }
+        i += 6
+    }
+    return true
+}
